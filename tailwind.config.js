70c9f35a22660f13631c95e952a6b7f80c8d11eb
Federico.Murru <djfedemurru@icloud.com>
2024-08-22 17:44:36 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,21 +10,19 @@ module.exports = {
 			colors: {
 				// Background colors
 				bg: {
-					dark: "#111827", // Black background for the main layout
-					light: "#1a1a1a", // Slightly lighter background for sections
+					dark: "#111827",
+					light: "#1a1a1a",
 				},
 				// Text colors
 				text: {
-					primary: "#f5f5f5", // Light text color for readability on dark background
-					secondary: "#dcdcdc", // Slightly darker text for secondary elements
-					// Accent color for highlights and important text
+					primary: "#f5f5f5",
+					secondary: "#dcdcdc",
 				},
 				// Additional colors
-				primary: "#111827", // Primary color for buttons, links, etc.
-				secondary: "#333333", // Secondary color for less prominent elements
-				success: "#28a745", // Green for success messages or notifications
-				error: "#dc3545", // Red for error messages or notifications
-				warning: "#ffc107", // Yellow for warnings or alerts
+				primary: "#111827",
+				secondary: "#333333",
+				success: "#28a745",
+				warning: "#ffc107",
 			},
 		},
 	},
