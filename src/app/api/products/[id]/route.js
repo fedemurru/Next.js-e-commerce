9bf5392cba3eec684709fa459bcbedef77fb92dc
Federico.Murru <djfedemurru@icloud.com>
2024-08-27 09:09:55 +0100

    error handling fixed

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -2,16 +2,47 @@ import { NextResponse } from "next/server";
 import { data } from "../data";
 
 const findById = (id) => {
-	return data.find((el) => el.id === id);
+	return data.find((el) => {
+		return el.id == id;
+	});
 };
 
-export async function GET(req, { params }) {
-	const id = Number(params.id);
-	const result = findById(id);
-	if (!result) {
-		return NextResponse.json({
-			message: `product with id of ${params.id} does not exist`,
-		});
+export const GET = (req, { params }) => {
+	try {
+		const id = Number(params.id);
+		if (!id) {
+			return NextResponse.json(
+				{
+					message: `invalid product with id`,
+				},
+				{ status: 400 }
+			);
+		}
+		const result = findById(id); // null
+
+		if (!result) {
+			return NextResponse.json(
+				{
+					message: `product with id of ${params.id} does not exist`,
+				},
+				{ status: 404 }
+			);
+		}
+
+		return NextResponse.json(result);
+	} catch (error) {
+		const errorArray = error.toString().split(":");
+		const errorType = errorArray[0];
+		const cause = errorArray[1];
+		return NextResponse.json(
+			{
+				error: {
+					message: cause,
+					code: 500,
+					cause: errorType,
+				},
+			},
+			{ status: 500 }
+		);
 	}
-	return NextResponse.json(result);
-}
+};
