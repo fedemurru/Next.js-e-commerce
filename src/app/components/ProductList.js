2b48845ec8bfd3f847a6a9f1f561f6942bd940c0
Federico.Murru <djfedemurru@icloud.com>
2024-08-28 07:55:06 +0100

    not found

--- src/app/components/ProductList.js
+++ src/app/components/ProductList.js
@@ -4,48 +4,54 @@ import { fetchFilteredProducts } from "./lib/fetchFilteredProducts";
 
 export default async function ProductList({ query }) {
 	try {
-		const filter = await fetchFilteredProducts(query);
-		console.log(filter);
+		const filtered = fetchFilteredProducts(query);
+		// console.log(filter);
 
 		return (
 			<div className="flex justify-center px-4 mt-28">
 				<div className="flex flex-wrap justify-center gap-6 max-w-7xl w-full">
-					{filter.map(({ id, name, price }) => (
-						<div
-							key={id}
-							className="bg-white border rounded-md shadow-md flex flex-col items-center"
-							style={{ width: "250px", height: "300px" }}
-						>
-							<Link href={`/products/${id}`}>
-								<div
-									className="w-full h-full flex flex-col border-yellow-400"
-									style={{ width: "250px", height: "300px" }}
-								>
-									<div className="relative w-full h-full overflow-hidden">
-										<Image
-											src="/photo.jpg"
-											alt={`Image of ${name}`}
-											layout="fill"
-											objectFit="cover"
-											className="object-cover"
-										/>
+					{filtered.length > 0 ? (
+						filtered.map(({ id, name, price }) => (
+							<div
+								key={id}
+								className="bg-white border rounded-md shadow-md flex flex-col items-center"
+								style={{ width: "250px", height: "300px" }}
+							>
+								<Link href={`/products/${id}`}>
+									<div
+										className="w-full h-full flex flex-col border-yellow-400"
+										style={{ width: "250px", height: "300px" }}
+									>
+										<div className="relative w-full h-full overflow-hidden">
+											<Image
+												src="/photo.jpg"
+												alt={`Image of ${name}`}
+												layout="fill"
+												objectFit="cover"
+												className="object-center"
+											/>
+										</div>
+										<div className="w-full h-1/3 flex flex-col items-center p-4">
+											<h3 className="text-sm text-gray-700">{name}</h3>
+											{price ? (
+												<p className="text-sm font-medium text-gray-900 mt-2">
+													{price}
+												</p>
+											) : (
+												<p className="text-sm font-medium text-red-500 mt-2">
+													Price Unavailable
+												</p>
+											)}
+										</div>
 									</div>
-									<div className="w-full h-1/3 flex flex-col items-center p-4">
-										<h3 className="text-sm text-gray-700">{name}</h3>
-										{price ? (
-											<p className="text-sm font-medium text-gray-900 mt-2">
-												{price}
-											</p>
-										) : (
-											<p className="text-sm font-medium text-red-500 mt-2">
-												Price Unavailable
-											</p>
-										)}
-									</div>
-								</div>
-							</Link>
+								</Link>
+							</div>
+						))
+					) : (
+						<div className="flex justify-center items-center h-96">
+							<p className="text-gray-500">No products found</p>
 						</div>
-					))}
+					)}
 				</div>
 			</div>
 		);
--- src/app/components/lib/fetchFilteredProducts.js
+++ src/app/components/lib/fetchFilteredProducts.js
@@ -1,13 +1,30 @@
 import { data } from "@/app/api/products/data"; // Adjust path as needed
 
-export async function fetchFilteredProducts(query) {
-	// Simulate network delay
-	await new Promise((resolve) => setTimeout(resolve, 1000));
-
-	// Filter data based on the query in the product name
+export function fetchFilteredProducts(query) {
 	const filteredProducts = data.filter((product) =>
-		product.name.toLowerCase().includes(query.toLowerCase())
+		product.name.toLowerCase().startsWith(query.toLowerCase())
 	);
 
 	return filteredProducts;
 }
+
+// export async function fetchFilteredProducts(query) {
+// 	try {
+// 		// If the query is empty, fetch all products or handle it accordingly
+// 		const url = query
+// 			? `http://localhost:3000//api/products?query=${encodeURIComponent(query)}`
+// 			: `http://localhost:3000//api/products`;
+
+// 		// Use a relative URL for flexibility in different environments
+// 		const res = await fetch(url, {
+// 			cache: "no-store",
+// 		});
+
+// 		const products = await res.json();
+
+// 		return products;
+// 	} catch (error) {
+// 		console.error("Error fetching product data:", error.message);
+// 		throw error;
+// 	}
+// }
