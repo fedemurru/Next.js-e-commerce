12f7271602b1b6aaf00675d5d06472cdf3071d3e
Federico.Murru <djfedemurru@icloud.com>
2024-08-29 10:28:52 +0100

    filter final

--- src/app/components/searchInput.js
+++ src/app/components/searchInput.js
@@ -19,6 +19,12 @@ function Search() {
 		router.replace(`${pathname}?${params.toString()}`);
 	}
 
+	function handleKeyDown(event) {
+		if (event.key === "Enter") {
+			handleSearch(event.target.value);
+		}
+	}
+
 	return (
 		<div>
 			<input
@@ -26,7 +32,7 @@ function Search() {
 				id="search-navbar"
 				className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 				placeholder="Search..."
-				onChange={(e) => handleSearch(e.target.value)}
+				onKeyDown={handleKeyDown}
 				defaultValue={searchParams.get("query") || ""}
 			/>
 		</div>
