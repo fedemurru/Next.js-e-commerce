f62ce016e08ea9c1b317b578d4bdfb83cd79be51
Federico.Murru <djfedemurru@icloud.com>
2024-09-11 08:59:38 +0100

    seed prisma

---
+++ prisma/migrations/20240911075440_add_product_table/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `User`;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,25 +1,36 @@
 import { NextResponse } from "next/server";
 import { data } from "./data";
+import { PrismaClient } from "@prisma/client";
 
-export const GET = (req, res) => {
-	try {
-		if (!data) {
-			return NextResponse.json(
-				{ message: "No data available" },
-				{ status: 404 }
-			);
-		}
+const prisma = new PrismaClient();
 
-		return NextResponse.json(data);
-	} catch (error) {
-		console.error("Error handling GET request:", error);
-
-		return NextResponse.json(
-			{
-				message: "An error occurred while processing your request.",
-				cause: error.message || "Unknown error",
-			},
-			{ status: 500 }
-		);
+export default async function handler(req, res) {
+	if (req.method === "GET") {
+		const users = await prisma.user.findMany();
+		res.json(users);
+		console.log(users);
 	}
-};
+}
+
+// export const GET = (req, res) => {
+// 	try {
+// 		if (!data) {
+// 			return NextResponse.json(
+// 				{ message: "No data available" },
+// 				{ status: 404 }
+// 			);
+// 		}
+
+// 		return NextResponse.json(data);
+// 	} catch (error) {
+// 		console.error("Error handling GET request:", error);
+
+// 		return NextResponse.json(
+// 			{
+// 				message: "An error occurred while processing your request.",
+// 				cause: error.message || "Unknown error",
+// 			},
+// 			{ status: 500 }
+// 		);
+// 	}
+// };
