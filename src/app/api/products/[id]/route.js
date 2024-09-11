1b60395331e24b99fe23a2252d8b19f416cd03e7
Federico.Murru <djfedemurru@icloud.com>
2024-09-11 11:58:20 +0100

    db ok

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -7,20 +7,29 @@ const findById = (id) => {
 	});
 };
 
-export const GET = (req, { params }) => {
+// /src/app/api/products/[id]/route.js
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(req, { params }) {
 	try {
 		const id = Number(params.id);
-		if (!id) {
+
+		if (isNaN(id)) {
 			return NextResponse.json(
 				{
-					message: `Invalid id`,
+					message: "Invalid id",
 				},
 				{ status: 400 }
 			);
 		}
-		const result = findById(id);
 
-		if (!result) {
+		const product = await prisma.product.findUnique({
+			where: { id: id },
+		});
+
+		if (!product) {
 			return NextResponse.json(
 				{
 					message: `Product with id of ${params.id} does not exist`,
@@ -29,7 +38,7 @@ export const GET = (req, { params }) => {
 			);
 		}
 
-		return NextResponse.json(result);
+		return NextResponse.json(product);
 	} catch (error) {
 		const errorArray = error.toString().split(":");
 		const errorType = errorArray[0] || "Unknown Error";
@@ -44,5 +53,47 @@ export const GET = (req, { params }) => {
 			},
 			{ status: 500 }
 		);
+	} finally {
+		await prisma.$disconnect();
 	}
-};
+}
+
+// export const GET = (req, { params }) => {
+// 	try {
+// 		const id = Number(params.id);
+// 		if (!id) {
+// 			return NextResponse.json(
+// 				{
+// 					message: `Invalid id`,
+// 				},
+// 				{ status: 400 }
+// 			);
+// 		}
+// 		const result = findById(id);
+
+// 		if (!result) {
+// 			return NextResponse.json(
+// 				{
+// 					message: `Product with id of ${params.id} does not exist`,
+// 				},
+// 				{ status: 404 }
+// 			);
+// 		}
+
+// 		return NextResponse.json(result);
+// 	} catch (error) {
+// 		const errorArray = error.toString().split(":");
+// 		const errorType = errorArray[0] || "Unknown Error";
+// 		const cause = errorArray[1] || "An unexpected error occurred";
+// 		return NextResponse.json(
+// 			{
+// 				error: {
+// 					message: cause,
+// 					code: 500,
+// 					cause: errorType,
+// 				},
+// 			},
+// 			{ status: 500 }
+// 		);
+// 	}
+// };
