87e7673f28a4a81a635089f17414a7d9b6a2ac82
Federico.Murru <djfedemurru@icloud.com>
2024-09-11 11:55:49 +0100

    db

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,4 +1,5 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+// import { data } from "./data";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
