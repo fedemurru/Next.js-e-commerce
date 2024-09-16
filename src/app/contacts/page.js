9d2e3d327cd102c8cd19bd514fc16b11083b2b1a
Federico.Murru <djfedemurru@icloud.com>
2024-09-16 15:28:25 +0100

    .

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
+import Navbar from "../components/Navbar";
 
 const page = () => {
 	return (
 		<div className="min-h-screen flex items-center justify-center ">
+			<Navbar />
 			<ContactForm />
 		</div>
 	);
